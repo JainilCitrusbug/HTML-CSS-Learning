694857a5f455587d9a71f8b782e26098144e50a5
JainilCitrusbug <99866938+JainilCitrusbug@users.noreply.github.com>
2022-03-08 17:21:38 +0530

    Add files via upload

--- Beenolo/Assets/JS/main.js
+++ Beenolo/Assets/JS/main.js
@@ -1,10 +1,17 @@
 var button = document.getElementsByClassName('side');
 var div = document.getElementsByClassName('img-unicorn');
+var number = document.getElementsByClassName("side-number");
 console.log(button);
 console.log(div);
 var l = 0;
+button[0].style.opacity = "0.5";
+number[1].style.opacity = "0.5";
 
 button[1].onclick = ()=>{
+    button[0].style.opacity = "1";
+    button[1].style.opacity = "0.5";
+    number[0].style.opacity = "0.5";
+    number[1].style.opacity = "1";
     l++;
     for(var i of div){
         if (l==0) {i.style.left = "0px";}
@@ -14,6 +21,10 @@ button[1].onclick = ()=>{
 }
 
 button[0].onclick = ()=>{
+    button[0].style.opacity = "0.5";
+    button[1].style.opacity = "1";
+    number[0].style.opacity = "1";
+    number[1].style.opacity = "0.5";
     l--;
     for(var i of div){
         if (l==0) {i.style.left = "0px";}
